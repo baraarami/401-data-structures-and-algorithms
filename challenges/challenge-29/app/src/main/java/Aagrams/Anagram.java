7fa867c93ba057b8d4ed3c4bf3fcfe21217bc60f
baraarami <baraaajohmani20@gmail.com>
2021-08-29 18:01:21 +0300

    anagram

---
+++ challenges/challenge-29/app/src/main/java/Aagrams/Anagram.java
@@ -0,0 +1,2 @@
+package Aagrams;public class Anagram {
+}
---
+++ challenges/challenge-41/Aagrams/Anagram.java
@@ -0,0 +1,33 @@
+package Aagrams;
+
+
+import java.util.Arrays;
+
+public class Anagram {
+    public static void main(String[] args) {
+        String x= "Software";
+        String y="Swear often";
+
+        x= x.replace(" " , "");
+        y= y.replace(" " , "");
+
+        x = x.toLowerCase();
+        y= y.toLowerCase();
+
+        char a [] = x.toCharArray();
+        char b [] = y.toCharArray();
+
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        Boolean result = Arrays.equals(a , b);
+        if (result == true){
+            System.out.println(true);
+        }else{
+            System.out.println(false);
+        }
+
+
+
+    }
+}
